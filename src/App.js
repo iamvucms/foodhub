5885835da4b2f5ff2034a153e61a477c56ea7d8f
Vu Nguyen <vucms@VuNguyen.local>
2022-03-16 13:23:26 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
